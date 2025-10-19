3bd026e3198edae469cf26685fd7f637818b75ca
Emin Cantürk <109080090+canturkemin@users.noreply.github.com>
2025-10-20 02:45:11 +0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,43 @@
 // src/firebase.ts
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+// import { initializeApp } from 'firebase/app'
+// import { getFirestore } from 'firebase/firestore'
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCd4OjBIDfsZ5EltD0JQvvubZuMpMAXB9o",
+//   authDomain: "be-chair-data-collection.firebaseapp.com",
+//   projectId: "be-chair-data-collection",
+//   storageBucket: "be-chair-data-collection.firebasestorage.app",
+//   messagingSenderId: "602864192656",
+//   appId: "1:602864192656:web:6844f0dc3f98c4e93f1c15",
+//   measurementId: "G-S1MB8CMM54"
+// };
+
+// const app = initializeApp(firebaseConfig)
+// const db = getFirestore(app)
+// 
+// export { db }
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCd4OjBIDfsZ5EltD0JQvvubZuMpMAXB9o",
-  authDomain: "be-chair-data-collection.firebaseapp.com",
-  projectId: "be-chair-data-collection",
-  storageBucket: "be-chair-data-collection.firebasestorage.app",
-  messagingSenderId: "602864192656",
-  appId: "1:602864192656:web:6844f0dc3f98c4e93f1c15",
-  measurementId: "G-S1MB8CMM54"
+  apiKey: "AIzaSyAb-_HFlt6jZOR5aMCKrg3uk7PAvzWa5_0",
+  authDomain: "shoe-customization.firebaseapp.com",
+  projectId: "shoe-customization",
+  storageBucket: "shoe-customization.firebasestorage.app",
+  messagingSenderId: "989806289479",
+  appId: "1:989806289479:web:97eb7e47f945e01fad0762",
+  measurementId: "G-1XVYP9HBK6"
 };
 
-const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export { db }
