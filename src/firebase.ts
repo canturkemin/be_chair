423587050a8003d34186906d6c1945b5a22d3742
Emin Cantürk <109080090+canturkemin@users.noreply.github.com>
2025-10-23 17:24:47 +0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -40,6 +40,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db = getFirestore(app)
+const db = getFirestore(app);
 
 export { analytics, db }
